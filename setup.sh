4136e9367ff895ff53b1ce417db1503d8bb2f699
mansande <mansande@kth.se>
2024-10-26 17:05:47 +0200

    rm python

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
-#
-#/bin/bash
-#
-#
+#!/bin/bash
+
+export DACE_PATH=/home/mansande/Develop/scivenv/lib/python3.12/site-packages/dace/runtime/
 export DACE_compiler_cpu_executable=/usr/bin/clang++
+
+#set up neko and dep.
 NEKO_PATH=submodules/neko/install_neko
 JSONFORTRAN_PATH=submodules/json-fortran/install_jsonfortran
-export DACE_PATH=/home/mansande/Develop/scivenv/lib/python3.12/site-packages/dace/runtime/
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PWD/$JSONFORTRAN_PATH/lib/pkgconfig
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PWD/$NEKO_PATH/lib/pkgconfig
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/$JSONFORTRAN_PATH/lib
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/src/cpp/include
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/src/cpp/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/src/cpp
